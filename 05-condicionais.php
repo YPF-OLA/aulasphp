ae998881719c1018ab857c4bb4660c97c3b98b21
YGOR PEREIRA DE FARIA <ygor.pfaria135@gmail.com>
2025-02-04 11:34:06 -0300

    PHP - Condicionais

---
+++ 05-condicionais.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP - Condicionais</title>
+</head>
+<body>
+    <h1>Estruturas Condicionais</h1>
+    <hr>
+
+    <h2>Simples</h2>
+
+    <h2>Completa</h2>
+
+    <h2>Encadeada</h2>
+</body>
+</html>
