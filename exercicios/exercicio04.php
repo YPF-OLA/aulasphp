fa8e8c5282b491150b0310629f4a724c3fa78f12
YGOR PEREIRA DE FARIA <ygor.pfaria135@gmail.com>
2025-02-10 11:20:12 -0300

    usei bootstrap

--- exercicios/exercicio04.php
+++ exercicios/exercicio04.php
@@ -5,58 +5,17 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Loops</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <style>
-      
-        table {
-            width: 50%;
-            background-color: #fff;
-        }
-
-        th, td {
-            padding: 12px 20px;
-            text-align: center;
-            font-size: 16px;
-        }
-
-        th {
-            background-color: gray;
-            color: white;
-            text-transform: uppercase;
-        }
-
-        tr:nth-child(even) {
-            background-color: lightgray;
-        }
-
-        tr:hover {
-            background-color: lightgray;
-            cursor: pointer;
-        }
-
-        td {
-            color: black;
-        }
-
-
-        @media (max-width: 768px) {
-            table {
-                width: 100%;
-            }
-            th, td {
-                padding: 10px;
-            }
-        }
-    </style>
+    
 </head>
 <body>
-    <h1>Estrutura de Dados</h1>
-    <table>
+    <h1 class="text-center">Estrutura de Dados</h1>
+    <table class="table table-bordered table table-striped table-hover table table-dark table-striped">
         <caption><b>Manipulação de dados</b></caption>
         <thead>
             <tr>
-                <th><b>ID</b></th>
-                <th><b>Linguagem</b></th>
-                <th><b>Descrição</b></th>
+                <th scope="col"><b>ID</b></th>
+                <th scope="col"><b>Linguagem</b></th>
+                <th scope="col"><b>Descrição</b></th>
             </tr>
         </thead>
         <tbody>
